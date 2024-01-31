48c7d6393b5b677d953510fbaba4e9f2d84589cd
Kelimon <wirklichneue@hotmail.com>
2024-01-31 16:44:40 +0100

    erster commit

---
+++ src/views/examples/LandingPage.js
@@ -0,0 +1,277 @@
+/*!
+
+=========================================================
+* BLK Design System React - v1.2.2
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/blk-design-system-react
+* Copyright 2023 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/blk-design-system-react/blob/main/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+// react plugin used to create charts
+import { Line } from "react-chartjs-2";
+// reactstrap components
+import {
+  Button,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  CardTitle,
+  ListGroupItem,
+  ListGroup,
+  Container,
+  Row,
+  Col,
+} from "reactstrap";
+
+// core components
+import ExamplesNavbar from "components/Navbars/ExamplesNavbar.js";
+import Footer from "components/Footer/Footer.js";
+
+import bigChartData from "variables/charts.js";
+
+export default function LandingPage() {
+  React.useEffect(() => {
+    document.body.classList.toggle("landing-page");
+    // Specify how to clean up after this effect:
+    return function cleanup() {
+      document.body.classList.toggle("landing-page");
+    };
+  }, []);
+  return (
+    <>
+      <ExamplesNavbar />
+      <div className="wrapper">
+        <div className="page-header">
+          <img
+            alt="..."
+            className="path"
+            src={require("assets/img/blob.png")}
+          />
+          <img
+            alt="..."
+            className="path2"
+            src={require("assets/img/path2.png")}
+          />
+          <img
+            alt="..."
+            className="shapes triangle"
+            src={require("assets/img/triunghiuri.png")}
+          />
+          <img
+            alt="..."
+            className="shapes wave"
+            src={require("assets/img/waves.png")}
+          />
+          <img
+            alt="..."
+            className="shapes squares"
+            src={require("assets/img/patrat.png")}
+          />
+          <img
+            alt="..."
+            className="shapes circle"
+            src={require("assets/img/cercuri.png")}
+          />
+          <div className="content-center">
+            <Row className="row-grid justify-content-between align-items-center text-left">
+              <Col lg="7" md="5">
+                <h1 className="text-white">
+                  Entfessel dein  <br />
+                  <span className="text-white">Lernpotential</span>
+                </h1>
+                <p className="text-white mb-3">
+                Erfolg in der Schule und im Studium beginnt hier! Mit maßgeschneiderter Nachhilfe, die auf Deine individuellen Bedürfnisse zugeschnitten ist, helfen wir Dir, Deine Ziele zu erreichen. Unsere erfahrenen Tutorinnen und Tutoren bieten Unterstützung in einer Vielzahl von Fächern - von Mathematik bis Sprachen, von der Grundschule bis zum Abitur. Steigere Dein Wissen und Deine Noten mit uns.
+                </p>
+                <div className="btn-wrapper mb-3">
+                  <p className="category text-success d-inline">
+                    Günstig ab 10Є pro Nachhilfestunde!
+                  </p>
+                  <Button
+                    className="btn-link"
+                    color="success"
+                    href="#pablo"
+                    onClick={(e) => e.preventDefault()}
+                    size="sm"
+                  >
+                    <i className="tim-icons icon-minimal-right" />
+                  </Button>
+                </div>
+                <div className="btn-wrapper">
+                  <div className="button-container">
+                    <Button
+                      className="btn-icon btn-simple btn-round btn-neutral"
+                      color="default"
+                      href="#pablo"
+                      onClick={(e) => e.preventDefault()}
+                    >
+                      <i className="fab fa-twitter" />
+                    </Button>
+                    <Button
+                      className="btn-icon btn-simple btn-round btn-neutral"
+                      color="default"
+                      href="#pablo"
+                      onClick={(e) => e.preventDefault()}
+                    >
+                      <i className="fab fa-dribbble" />
+                    </Button>
+                    <Button
+                      className="btn-icon btn-simple btn-round btn-neutral"
+                      color="default"
+                      href="#pablo"
+                      onClick={(e) => e.preventDefault()}
+                    >
+                      <i className="fab fa-facebook" />
+                    </Button>
+                  </div>
+                </div>
+              </Col>
+<Col lg="4" md="5">
+  <img
+    alt="..."
+    className="img-fluid profile-image"
+    src={require("assets/img/Profilbild.jpg")}
+    style={{
+      boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2)',
+      borderRadius: '35px', // Runde Ecken
+      border: '5px solid #1C1E45', // Weißer Rand um das Bild
+    }}
+  />
+</Col>
+
+            </Row>
+          </div>
+        </div>
+        <section className="section section-lg">
+          <section className="section">
+            <img
+              alt="..."
+              className="path"
+              src={require("assets/img/path4.png")}
+            />
+            <Container>
+              <Row className="row-grid justify-content-between">
+              <Col md="6">
+                  <div className="pl-md-5">
+                    <h1>
+                      PPLUSS <br />
+                      hat bereits
+                    </h1>
+                    <p>
+                      
+                    </p>
+                    <br />
+                    <p>
+                      
+                    </p>
+                    <br />
+                    
+                  </div>
+                </Col>
+                <Col className="mt-lg-5" md="5">
+                  <Row>
+                    
+                    <Col className="px-2 py-2" lg="6" sm="12">
+                      <Card className="card-stats">
+                        <CardBody>
+                          <Row>
+                            <Col md="4" xs="5">
+                              <div className="icon-big text-center icon-warning">
+                                <i className="tim-icons icon-trophy text-warning" />
+                              </div>
+                            </Col>
+                            <Col md="8" xs="7">
+                              <div className="numbers">
+                                <CardTitle tag="p">3,237</CardTitle>
+                                <p />
+                                <p className="card-category">Schülern geholfen</p>
+                              </div>
+                            </Col>
+                          </Row>
+                        </CardBody>
+                      </Card>
+                    </Col>
+                    <Col className="px-2 py-2" lg="6" sm="12">
+                      <Card className="card-stats upper bg-default">
+                        <CardBody>
+                          <Row>
+                            <Col md="4" xs="5">
+                              <div className="icon-big text-center icon-warning">
+                                <i className="tim-icons icon-coins text-white" />
+                              </div>
+                            </Col>
+                            <Col md="8" xs="7">
+                              <div className="numbers">
+                                <CardTitle tag="p">3,653</CardTitle>
+                                <p />
+                                <p className="card-category">Geld verdient</p>
+                              </div>
+                            </Col>
+                          </Row>
+                        </CardBody>
+                      </Card>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col className="px-2 py-2" lg="6" sm="12">
+                      <Card className="card-stats">
+                        <CardBody>
+                          <Row>
+                            <Col md="4" xs="5">
+                              <div className="icon-big text-center icon-warning">
+                                <i className="tim-icons icon-gift-2 text-info" />
+                              </div>
+                            </Col>
+                            <Col md="8" xs="7">
+                              <div className="numbers">
+                                <CardTitle tag="p">593</CardTitle>
+                                <p />
+                                <p className="card-category">Geschenke vergeben</p>
+                              </div>
+                            </Col>
+                          </Row>
+                        </CardBody>
+                      </Card>
+                    </Col>
+                    
+                    <Col className="px-2 py-2" lg="6" sm="12">
+                      <Card className="card-stats">
+                        <CardBody>
+                          <Row>
+                            <Col md="4" xs="5">
+                              <div className="icon-big text-center icon-warning">
+                                <i className="tim-icons icon-credit-card text-success" />
+                              </div>
+                            </Col>
+                            <Col md="8" xs="7">
+                              <div className="numbers">
+                                <CardTitle tag="p">10,783</CardTitle>
+                                <p />
+                                <p className="card-category">Kreditkarten gescammed</p>
+                              </div>
+                            </Col>
+                          </Row>
+                        </CardBody>
+                      </Card>
+                    </Col>
+                  </Row>
+                </Col>
+                
+              </Row>
+            </Container>
+          </section>
+        </section>
+        <Footer/>
+      </div>
+    </>
+  );
+}
